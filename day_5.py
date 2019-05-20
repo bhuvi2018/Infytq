6b56f5620e4896b98d087016a748a4c5ff42d3b8
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-20 11:25:05 +0530

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -47,3 +47,25 @@ def find_winner_of_the_day(*match_tuple):
 #Invoke the function with each of the print statements given below
 print(find_winner_of_the_day("Team1","Team2","Team2","Team1","Team2"))
 #print(find_winner_of_the_day("Team1","Team2","Team1","Team2"))
+
+
+#PF-Exer-29
+def merge_lists(list1,list2):
+    #Write your logic here
+    new_merge_list=[]
+    for i in range(0,len(list1)):
+        new_merge_list.append(list1[i])
+    for i in range(0,len(list2)):
+        new_merge_list.append(list2[i])
+
+    return new_merge_list
+def sort_list(merged_list):
+    #Write your logic here
+    merged_list.sort()
+    return merged_list
+
+#Provide different values for list1 and list2 and test your program
+merged_list=merge_lists(list1=[1,2,3,4,1] ,list2=[2,3,4,5,4,6])
+print(merged_list)
+sorted_merged_list=sort_list(merged_list)
+print(sorted_merged_list)
