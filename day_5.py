0bd19b5d20c0e52d62c524659e2197f2b2c31e56
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-20 15:19:24 +0530

    Update day_5.py

--- day_5.py
+++ day_5.py
@@ -121,6 +121,47 @@ print(generate_frequency())
 print(sort_marks())
 
 
+#PF-Assgn-37
+
+#Global variables
+child_id=(10,20,30,40,50)
+chocolates_received=[12,5,3,4,6]
+
+def calculate_total_chocolates():
+    sum=0
+    for i in range(0,len(chocolates_received)):
+        sum+=chocolates_received[i]
+    return sum
+
+def reward_child(child_id_rewarded,extra_chocolates):
+    c=0
+    if(extra_chocolates<1):
+        print("Extra chocolates is less than 1")
+    else:
+        for i in range(0,len(child_id)):
+            if(child_id[i]==child_id_rewarded):
+                chocolates_received[i]+=extra_chocolates
+                c=1
+        if(c==1):
+            print(chocolates_received)
+        else:
+            print("Child id is invalid")
+
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+
+    #print("Extra chocolates is less than 1")
+    #print("Child id is invalid")
+    #print(chocolates_received)
+
+
+print(calculate_total_chocolates())
+#Test your code by passing different values for child_id_rewarded,extra_chocolates
+reward_child(20,2)
+
+
+
 #PF-Assgn-36
 def create_largest_number(number_list):
     ans=""
