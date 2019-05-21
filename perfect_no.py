72ea40313f8f4f4b6aba0f630f058d21c0dcfb51
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-21 21:00:22 +0530

    Update perfect_no.py

--- perfect_no.py
+++ perfect_no.py
@@ -9,3 +9,17 @@ if (sum1 == n):
     print("The number is a Perfect number!")
 else:
     print("The number is not a Perfect number!")
+
+    
+#Perfect number bet an interval
+
+llimit=int(input())
+ulimit = int(input())
+
+for n in range(llimit, ulimit + 1):
+    sum = 0
+    for divisor in range(1, n):
+        if not n % divisor:
+            sum += divisor
+    if sum==n:
+        print(n, "is a perfect number")
