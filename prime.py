5c858e61c13cc91fafdab238a9f2aa9ef7649359
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-21 20:46:13 +0530

    Update and rename prime_no_or_not.py to prime.py

--- prime.py
+++ prime.py
@@ -31,3 +31,21 @@ for num in range(lower,upper + 1):
                break
        else:
            print(num)
+
+      
+#Prime factor of a number
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i>=1:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return factors
+    
+print(prime_factors(315))
