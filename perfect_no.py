95e3b3ebdaa50d1033f71a3e368ac07d39426fcb
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-21 20:51:02 +0530

    Create perfect_no.py

---
+++ perfect_no.py
@@ -0,0 +1,11 @@
+#perfect number is a number whose divisors sum to the same number
+
+n = int(input())
+sum1 = 0
+for i in range(1, n):
+    if(n % i == 0):
+        sum1 = sum1 + i
+if (sum1 == n):
+    print("The number is a Perfect number!")
+else:
+    print("The number is not a Perfect number!")
