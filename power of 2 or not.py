74c3a177fcade7ce23fa6b7bfeba6018953627d0
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-21 21:39:59 +0530

    Create power of 2 or not.py

---
+++ power of 2 or not.py
@@ -0,0 +1,14 @@
+import math 
+def Log2(x): 
+    if x == 0: 
+        return false
+  
+    return (math.log10(x) / 
+            math.log10(2)) 
+def isPowerOfTwo(n): 
+    return (math.ceil(Log2(n)) == 
+            math.floor(Log2(n)))
+if(isPowerOfTwo(64)): 
+    print("Yes")
+else: 
+    print("No")
