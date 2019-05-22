656a1cb4e60b39823a5fb91aae56cf0a0fe6b097
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-22 09:59:06 +0530

    Update day_6.py

--- day_6.py
+++ day_6.py
@@ -9,3 +9,19 @@ def find_average(mark_list):
 
 m_list=[1,2,3,4]
 print("Average marks:", find_average(m_list))
+
+
+#PF-Assgn-40
+def is_palindrome(word):
+    l=word.lower()
+    r=l[::-1]
+    if(l==r):
+        return True
+    else:
+        return False
+result=is_palindrome("MadAMa")
+if(result):
+    print("The given word is a Palindrome")
+else:
+    print("The given word is not a Palindrome")
+
