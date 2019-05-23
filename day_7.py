4c0723a9272f0b00dca3cd3f183c5a2636dd7228
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-23 11:07:24 +0530

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -15,3 +15,13 @@ def guess_number(number_in_mind):
 
 #Provide different values for number_in_mind and test your program
 guess_number(4)
+
+
+#PF-Exer-34
+def find_number_of_combination(number_of_flavours):
+    total_combination=0
+    total_combination=2**number_of_flavours #power(**)
+    return total_combination
+
+number_of_combination=find_number_of_combination(6)
+print(number_of_combination)
