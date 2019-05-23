785c22ee9397f5ebe7358c1d5951f32bed891ff3
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-23 10:17:34 +0530

    Create day_7.py

---
+++ day_7.py
@@ -0,0 +1,17 @@
+#PF-Tryout
+
+def guess_number(number_in_mind):
+     # remove pass and write your logic here
+     if(number_in_mind<5):
+         print ('Number is low')
+     elif(number_in_mind>5):
+         print ('Number is high')
+     else:
+         print('You have got it right!!!')
+#use the print statements given below wherever applicable
+#print ('Number is low')
+#print ('Number is high')
+#print ('You have got it right!!!')
+
+#Provide different values for number_in_mind and test your program
+guess_number(4)
