b37fc76b546eff8887a74fd67e22e0f3048d145b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-23 16:25:41 +0530

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -25,3 +25,27 @@ def find_number_of_combination(number_of_flavours):
 
 number_of_combination=find_number_of_combination(6)
 print(number_of_combination)
+
+
+#PF-Exer-35
+
+def count_names(name_list):
+    count1=0
+    count2=0
+    for i in range(0,len(name_list)):
+        if(name_list[i].endswith("at") and name_list[i].find("at")!=0):
+            count1+=1
+        if(name_list[i].count("at")!=0):
+            count2+=1
+    #start writing your code here
+    #Populate the variables: count1 and count2
+
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    print("_at -> ",count1)
+    print("%at% -> ",count2)
+
+
+#Provide different names in the list and test your program
+name_list=[Hat, Cat, Rabbit, Matter]
+count_names(name_list)
