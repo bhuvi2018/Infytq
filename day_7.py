ed70939970ba9e2f66e7b94c23f17ee3ba9db7ac
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-23 17:45:55 +0530

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -96,3 +96,33 @@ def encrypt_sentence(sentence):
 sentence="The sun rises in the east"
 encrypted_sentence=encrypt_sentence(sentence)
 print(encrypted_sentence)
+
+
+#PF-Assgn-48
+
+def find_correct(word_dict):
+    c1=0
+    c2=0
+    c3=0
+    for key in word_dict:
+        if(key==word_dict[key]):
+            c1+=1
+        elif(len(key)==len(word_dict[key])):
+            c=0
+            for i in range(0,len(key)):
+                if(key[i]!=word_dict[key][i]):
+                    c+=1
+            if(c<=2):
+                c2+=1
+            else:
+                c3+=1
+        else:
+            c3+=1
+    list=[]
+    list.append(c1)
+    list.append(c2)
+    list.append(c3)
+    return list
+
+word_dict={"THREE": "TRICE", "MOST": "MICE", "GET": "GOT", "COME": "COME"}
+print(find_correct(word_dict))
