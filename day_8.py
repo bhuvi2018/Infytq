d8f54c6153d9653731063925e64a828f9c7cae7a
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 10:48:46 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -44,3 +44,32 @@ print(square_root(num3))
 
 square_root2= lambda x:math.sqrt(x)
 print(square_root2(num3))
+
+
+#PF-Exer-39
+#This verification is based on string match.
+
+principal_amount=4000
+duration=12
+rate_of_interest=13
+
+simple_interest = lambda x,y,z:(x*y*z)/100
+
+if(simple_interest(principal_amount,duration,rate_of_interest)>1000):
+    print("Platinum Member")
+else:
+    print("Gold Member")
+    
+    
+#PF-Exer-40
+#This verification is based on string match.
+
+num1=20
+num2=30
+
+div =lambda x,y:x+y
+
+if(div(num1,num2)%10)==0:
+    print("Divisible by 10")
+else:
+    print("Not Divisible by 10")
