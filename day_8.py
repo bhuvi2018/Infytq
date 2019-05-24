1711acaeb5cec0769f56d7c513701d9f9bde45ab
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 16:14:02 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -136,9 +136,9 @@ sample_data = range(1,11)
 print(sum_of_numbers(sample_data,None))
 
 
-#PF-Assgn-53 ERROR
+#PF-Assgn-53
 #This verification is based on string match.
-
+import re
 poem='''
 It takes strength for being certain,
 It takes courage to have doubt.
@@ -155,10 +155,33 @@ It takes courage to help if it is paining for someone.
 #Write your logic here for question 1
 
 print(poem.count("v"))
+print()
 print(poem.replace("\n",""))
 c=poem.replace("co","Co")
 c=c.replace("ch","Ch")
 print(c)
-poem=poem.replace("ai","ai*\*")
-poem=poem.replace("hi","hi*\*")
+poem=re.sub(r"ai...",r"ai*/*",poem)
+poem=re.sub(r"hi...",r"hi*/*",poem)
 print(poem)
+
+
+#PF-Assgn-54
+def check_anagram(data1,data2):
+    data2=data2.lower()
+    data1=data1.lower()
+    c=0
+    l=[]
+    s=""
+    if(len(data2)==len(data1)):
+        for i in range(0,len(data1)):
+            for j in range(0,len(data2)):
+                if(data2[j]==data1[i] and i!=j and not(j in l)):
+                    l.append(j)
+                    c+=1
+                    break
+    if(c==len(data2)):
+        return True
+    else:
+        return False
+    
+print(check_anagram("resell","scller"))
