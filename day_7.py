7f9540e49052f39fb65b5391d6f178d398a7ba8f
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 09:33:21 +0530

    Update day_7.py

--- day_7.py
+++ day_7.py
@@ -128,6 +128,32 @@ word_dict={"THREE": "TRICE", "MOST": "MICE", "GET": "GOT", "COME": "COME"}
 print(find_correct(word_dict))
 
 
+#PF-Assgn-50
+
+def sms_encoding(data):
+    new_list = []
+    final_list = []
+    final_ans = ""
+    new_list = data.split(" ")
+    vowels = ('a','e','i','o','u','A','E','I','O','U')
+    for i in range(0,len(new_list)):
+        count = 0
+        string = ""
+        for j in range(0,len(new_list[i])):
+            if new_list[i][j] in vowels:
+                count = count + 1
+                if(count==len(new_list[i])):
+                    final_list.append(new_list[i])
+            else: 
+                string = string + new_list[i][j]
+        if(len(string)!=0):
+            final_list.append(string)    
+    final_ans = " ".join(final_list)
+    return final_ans
+data="GOOD DAYS AND BAD DAYS"
+print(sms_encoding(data))
+
+
 #PF-Assgn-50 ERROR
 
 def sms_encoding(data):
@@ -142,10 +168,7 @@ def sms_encoding(data):
             if(c==len(i)):
                 break
             elif(j=="a" or j=="e" or j=="i" or j=="o" or j=="u"):
-                print(j)
-                print(i)
-                i.replace(j,"")
-                print(i)
+                i=i.replace(j,"")
     for i in list:
         s+=i
         if(i!=len(list)-1):
