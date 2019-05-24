eff1ce32c8d579021d075a07e204f6dedc6bece4
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 18:10:05 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -223,3 +223,26 @@ def max_frequency_word_counter(data):
 #Provide different values for data and test your program.
 data="Twinkle Twinkle little star little star"
 max_frequency_word_counter(data)
+
+
+#PF-Assgn-57
+def check_prime(number):
+    return number > 1 and all(number % i != 0 for i in range(2, number))
+    
+def rotations(num):
+    rotated = []
+    m = str(num)
+    for i in m:
+        rotated.append(int(m))
+        m = m[1:] + m[0]
+    return rotated
+
+def get_circular_prime_count(limit):
+    counter = 0 
+
+    for number in range(1, limit):
+        if all(check_prime(number) for number in rotations(number)): 
+            counter += 1 
+    return counter
+#Provide different values for limit and test your program
+print(get_circular_prime_count(1000))
