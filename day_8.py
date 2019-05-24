bc110b35f064cf91f8dddc4562cfaee9f100fca4
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 15:35:30 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -134,3 +134,31 @@ def odd(data):
 sample_data = range(1,11)
 
 print(sum_of_numbers(sample_data,None))
+
+
+#PF-Assgn-53 ERROR
+#This verification is based on string match.
+
+poem='''
+It takes strength for being certain,
+It takes courage to have doubt.
+It takes strength for challenging alone,
+It takes courage to lean on another.
+It takes strength for loving other souls,
+It takes courage to be loved.
+It takes strength for hiding our own pain,
+It takes courage to help if it is paining for someone.
+
+'''
+#Note: Triple quotes can be used to enclose Strings which has lines of text.
+
+#Write your logic here for question 1
+
+print(poem.count("v"))
+print(poem.replace("\n",""))
+c=poem.replace("co","Co")
+c=c.replace("ch","Ch")
+print(c)
+poem=poem.replace("ai","ai*\*")
+poem=poem.replace("hi","hi*\*")
+print(poem)
