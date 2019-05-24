5d35a20e94ea9aedcbd0b58f085e8d0077ff314d
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-24 17:32:08 +0530

    Update day_8.py

--- day_8.py
+++ day_8.py
@@ -186,3 +186,40 @@ def check_anagram(data1,data2):
         return False
     
 print(check_anagram("resell","scller"))
+
+
+#PF-Assgn-56
+
+def max_frequency_word_counter(data):
+    data = data.lower()
+    string_list = []
+    count_list = []
+    frequency = 0
+    max_length = 0
+    word = ""
+    str_list = data.split() 
+    unique_words = set(str_list) 
+      
+    for words in unique_words : 
+        string_list.append(words)
+        count_list.append(str_list.count(words))
+        
+    for i in range(0,len(count_list)):
+        if(count_list[i]>frequency):
+            frequency = count_list[i]
+            word = string_list[i]
+        elif(count_list[i]==frequency):
+            frequency = count_list[i]
+            if(len(string_list[i])>max_length):
+                max_length = len(string_list[i])
+                word = string_list[i]
+
+    
+    # Use the below given print statements to display the output
+    # Also, do not modify them for verification to work
+    print(word,frequency)
+
+
+#Provide different values for data and test your program.
+data="Twinkle Twinkle little star little star"
+max_frequency_word_counter(data)
