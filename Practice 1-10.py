2eb6227779f4f585609219d8cb4f2cc3b81834ef
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 10:32:53 +0530

    Update Practice 1-10.py

--- Practice 1-10.py
+++ Practice 1-10.py
@@ -100,3 +100,20 @@ def seed_no(number,ref_no):
 number=123
 ref_no=738
 print(seed_no(number,ref_no))
+
+
+#PF-Prac-8
+def calculate_net_amount(trans_list):
+    net_amount=0
+    for i in trans_list:
+        a=[]
+        a=i.split(":")
+        if(a[0]=='D'):
+            net_amount+=int(a[1])
+        else:
+            net_amount-=int(a[1])
+    
+    return net_amount
+
+trans_list=["D:300","D:200","W:200","D:100"]
+print(calculate_net_amount(trans_list))
