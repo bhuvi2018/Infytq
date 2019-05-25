1c439165ee64b0d31fb65f5e2eeb27ba6a05162a
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 12:07:19 +0530

    Update Problem 21-30.py

--- Problem 21-30.py
+++ Problem 21-30.py
@@ -48,3 +48,14 @@ def find_gcd(num1,num2):
 num1=45
 num2=9
 print(find_gcd(num1,num2))
+
+
+#PF-Prac-25
+def list_of_count(word_list):
+    count_list=[]
+    for i in word_list:
+        count_list.append(len(i))
+    return count_list
+
+word_list=["COme","here"]
+print(list_of_count(word_list))
