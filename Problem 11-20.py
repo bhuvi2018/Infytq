a5d14e2dc8a707e810cf79b37e1f0b856a0c4dc5
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 16:37:08 +0530

    Update Problem 11-20.py

--- Problem 11-20.py
+++ Problem 11-20.py
@@ -76,3 +76,17 @@ def check_22(num_list):
     return False
         
 print(check_22([3,2,5,1,2,1,2,2]))
+
+
+#PF-Prac-16
+def rotate_list(input_list,n):
+    output_list=[]
+    for i in range(len(input_list)-n,len(input_list)):
+        output_list.append(input_list[i])
+    for i in range(0,len(input_list)-n):
+        output_list.append(input_list[i])
+    return output_list
+
+input_list= [1,2,3,4,5,6]
+output_list=rotate_list(input_list,4)
+print(output_list)
