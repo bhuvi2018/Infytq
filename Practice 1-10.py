a5531583f077a14810b9ed52ec9638eed6f23685
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 10:41:44 +0530

    Update Practice 1-10.py

--- Practice 1-10.py
+++ Practice 1-10.py
@@ -131,3 +131,16 @@ def generate_dict(number):
 
 number=10
 print(generate_dict(number))
+
+
+#PF-Prac-10
+def string_both_ends(input_string):
+	if(len(input_string)<2):
+	    return -1
+	else:
+	    s=""
+	    s=input_string[0:2]+input_string[len(input_string)-2:len(input_string)]
+	    return s
+
+input_string="w3"
+print(string_both_ends(input_string))
