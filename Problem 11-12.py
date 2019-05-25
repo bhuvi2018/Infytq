0db1aa7ec1797389326820d5e24aa24cc7248b4d
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 11:02:00 +0530

    Create Problem 11-12.py

---
+++ Problem 11-12.py
@@ -0,0 +1,33 @@
+#PF-Prac-11
+def find_upper_and_lower(sentence):
+    #start writing your code here
+    up=0
+    lo=0
+    for i in sentence:
+        if(i>="a" and i<="z"):
+            lo+=1
+        if(i>="A" and i<="Z"):
+            up+=1
+    result_list=[]
+    result_list.append(up)
+    result_list.append(lo)
+
+    return result_list
+
+sentence="Come Here"
+print(find_upper_and_lower(sentence))
+
+
+#PF-Prac-12
+def generate_sentences(subjects,verbs,objects):
+    sentence_list=[]
+    for i in subjects:
+        for j in verbs:
+	        for k in objects:
+	            sentence_list.append(i+" "+j+" "+k)
+    return sentence_list
+
+subjects=["I","You"]
+verbs=["love", "play"]
+objects=["Hockey","Football"]
+print(generate_sentences(subjects,verbs,objects))
