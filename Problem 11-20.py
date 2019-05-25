af7bfdddcaa7fe171b0d18d16a4213c876d9afed
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 11:37:18 +0530

    Update Problem 11-20.py

--- Problem 11-20.py
+++ Problem 11-20.py
@@ -31,3 +31,22 @@ subjects=["I","You"]
 verbs=["love", "play"]
 objects=["Hockey","Football"]
 print(generate_sentences(subjects,verbs,objects))
+
+
+#PF-Tryout
+def find_five_digit():
+    num2=0
+    num3=0
+    num4=0
+    num5=0
+    for i in range(9,-1,-1):
+        num2=int(i-2)
+        num3=int(num2-2)
+        num4=int(num3-2)
+        num5=int(num3)
+        if(num3+num4+num5==i and num2+num3+num4+num5+i==19):
+            break
+    s=str(i)+str(num2)+str(num3)+str(num4)+str(num5)
+    print(s)
+
+find_five_digit()
