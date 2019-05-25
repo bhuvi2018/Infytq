9f4168a5d8cedaca017501fc15c87295c5b342fe
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 10:17:58 +0530

    Update Practice 1-10.py

--- Practice 1-10.py
+++ Practice 1-10.py
@@ -70,3 +70,16 @@ def count_digits_letters(sentence):
 
 sentence="Infosys Mysore 570027"
 print(count_digits_letters(sentence))
+
+
+
+#PF-Prac-6
+def list123(nums):
+    for i in range(0,len(nums)-1):
+        if(nums[i]==1 and nums[i+1]==2 and nums[i+2]==3):
+                return True
+    return False
+
+    
+nums=[1,2,3,4,5]
+print(list123(nums))
