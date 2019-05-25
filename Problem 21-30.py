e77991f6df5c126d16c937fb24acb78f81ddc5f5
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 14:45:25 +0530

    Update Problem 21-30.py

--- Problem 21-30.py
+++ Problem 21-30.py
@@ -97,3 +97,26 @@ def sing_99_bottles():
         print(" bottles of beer on the wall.")
    
 sing_99_bottles()
+
+
+#PF-Prac-29
+def exchange_list(number_list):
+    #start writing your code here
+    d=len(number_list)
+    a=len(number_list)//2
+    b=a//2+a
+    k=0
+    for i in range(a,b):
+        c=number_list[i]
+        number_list[i]=number_list[d-k-1]
+        number_list[d-k-1]=c
+    for i in range(a):
+        temp=number_list[i]
+        number_list[i]=number_list[i+a]
+        number_list[i+a]=temp
+    return number_list
+
+    
+     
+number_list=[1,2,3,4,5,6]
+print(exchange_list(number_list))
