770883262939d74e015828fba81951b3ad841d3a
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 16:52:03 +0530

    Update Problem 21-30.py

--- Problem 21-30.py
+++ Problem 21-30.py
@@ -1,3 +1,19 @@
+#PF-Prac-21
+def check_numbers(num1,num2):
+    #start writing your code here
+    num_list=[]
+    for i in range(num1,num2+1):
+        for j in range(num1,num2+1):
+            if(i%j==0 and i not in num_list and i!=j):
+                num_list.append(i)
+    count=len(num_list)
+    return [num_list,count]
+
+num1=2
+num2=20
+print(check_numbers(num1, num2))
+
+
 '''
 Write a python function to print the given number of diagonal lines of stars.
 Sample input: 5
