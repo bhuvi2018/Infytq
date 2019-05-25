e2f786ac143ac693e4af768c50be47606d289b52
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 14:51:59 +0530

    Create Practices 31-40.py

---
+++ Practices 31-40.py
@@ -0,0 +1,16 @@
+#PF-Prac-31
+def sum_of_elements(num_list,number):
+    a=len(num_list)
+    result_sum=0
+    for i in range(1,a-1):
+        if(num_list[i-1]!=number and num_list[i+1]!=number and num_list[i]!=number):
+            result_sum+=num_list[i]
+    if(num_list[1]!=number and num_list[0]!=number):
+        result_sum+=num_list[0]
+    if(num_list[a-2]!=number and num_list[a-1]!=number):
+        result_sum+=num_list[a-1]
+    return result_sum
+      
+num_list=[1,2,1,2]
+number=2
+print(sum_of_elements(num_list, number))
