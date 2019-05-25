6d3d16fd7bbee4aa0eb99d303ee5b7d3a953b16e
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 10:14:49 +0530

    Update Practice 1-10.py

--- Practice 1-10.py
+++ Practice 1-10.py
@@ -53,3 +53,20 @@ def find_nine(nums):
 nums=[1,9,4,5,6]
 print(find_nine(nums))
 
+
+#PF-Prac-5
+def count_digits_letters(sentence):
+    l=[]
+    c1=0
+    c2=0
+    for i in sentence:
+        if(ord(i)>=65 and ord(i)<=90 or ord(i)>=97 and ord(i)<=122):
+            c1+=1
+        elif((ord(i)>=0 or ord(i)<=9) and not(i==" ")):            
+            c2+=1
+    l.append(c1)
+    l.append(c2)
+    return l
+
+sentence="Infosys Mysore 570027"
+print(count_digits_letters(sentence))
