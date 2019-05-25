afe6e4f8ecda64f6afd137b2a99d60ad64e0550b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 11:41:51 +0530

    Update Problem 11-20.py

--- Problem 11-20.py
+++ Problem 11-20.py
@@ -50,3 +50,13 @@ def find_five_digit():
     print(s)
 
 find_five_digit()
+
+
+#PF-Prac-15
+def check_22(num_list):
+    for i in range(0,len(num_list)-1):
+        if(num_list[i]==2 and num_list[i+1]==2):
+            return True
+    return False
+        
+print(check_22([3,2,5,1,2,1,2]))
