d45479a72831b3dfad78ff96cd982bf96583944c
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 11:58:46 +0530

    Update Problem 11-20.py

--- Problem 11-20.py
+++ Problem 11-20.py
@@ -60,3 +60,28 @@ def check_22(num_list):
     return False
         
 print(check_22([3,2,5,1,2,1,2,2]))
+
+'''
+Write a python function to print the given number of diagonal lines of stars.
+Sample input: 5
+
+Expected output: 
+* 
+.* 
+..* 
+...* 
+....* 
+Note: Setting the end parameter of the print function to empty string prevents the issuing of the new line.
+Example: print(".",end="") will maintain the cursor in the same line after displaying "."
+'''
+
+#PF-Tryout
+def diagonal_stars(number):
+   #start writing your code here
+    for i in range(0,number):
+        for j in range(i):
+            print(".",end="")
+        print("*")
+
+number=6    
+diagonal_stars(number)
