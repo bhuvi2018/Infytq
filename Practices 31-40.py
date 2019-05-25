ba4c9d285925dc971cfccf14619a8162d6860137
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 15:52:51 +0530

    Update Practices 31-40.py

--- Practices 31-40.py
+++ Practices 31-40.py
@@ -25,3 +25,33 @@ def sum_of_list(num_list):
 num_list=[44,23,77,11,89,3]
 result=sum_of_list(num_list)
 print("Sum of the elements:",result)
+
+
+#PF-Prac-38
+def build_index_grid(rows, columns):
+    result_list=[]
+    s=""
+    for i in range(0,rows):
+        for j in range(0,columns):
+            if(i==0 and j==0):
+                s=s+"["
+            if(j==0):
+                s=s+"["
+            s=s+"'"+str(i)+","+str(j)+"'"
+            if(j!=(columns-1)):
+                s=s+","
+            if(j==(columns-1) and j==(columns-1) and i!=(rows-1)):
+                s=s+"],"
+            if(j==(columns-1) and i==(rows-1)):
+                s=s+"]]"
+        result_list.append(s)
+        s=""
+    return result_list
+
+rows=4
+columns=3
+result=build_index_grid(rows,columns)
+print("Rows:",rows,"Columns:",columns)
+print("The matrix is:",result)
+for i in result:
+    print(i)
