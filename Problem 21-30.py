57bfc12da75b230bee96b3d3aa689af778cb3676
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 12:17:16 +0530

    Update Problem 21-30.py

--- Problem 21-30.py
+++ Problem 21-30.py
@@ -73,3 +73,13 @@ def check_occurence(string):
         
 string="Jet on the Mat but mat is too long"
 print(check_occurence(string))
+
+
+#PF-Prac-27
+def check_for_ten(num1,num2):
+    if(num1==10 or num2==10 or num1+num2==10):
+        return True
+    else:
+        return False
+    
+print(check_for_ten(10,9))
