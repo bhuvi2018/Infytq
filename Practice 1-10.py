4d3e5a5ef69c9d926799344a092ed8e6e29b0dd5
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-25 09:49:03 +0530

    Update Practice 1-10.py

--- Practice 1-10.py
+++ Practice 1-10.py
@@ -26,3 +26,17 @@ def bracket_pattern(input_str):
     
 input_str="(())("
 print(bracket_pattern(input_str))
+
+
+#PF-Prac-3
+
+def create_new_dictionary(prices):
+    new_dict={}
+    for key in prices:
+        if(prices[key]>200):
+            new_dict.__setitem__(key, prices[key])
+    
+    return new_dict
+
+prices = { 'ACME': 45.23,'AAPL': 612.78,'IBM': 205.55,'HPQ': 37.20,'FB': 10.75}
+print(create_new_dictionary(prices))
