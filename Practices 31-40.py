578d0ea77143a05886d1a8019f263ab459589bf9
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-26 20:04:50 +0530

    Update Practices 31-40.py

--- Practices 31-40.py
+++ Practices 31-40.py
@@ -28,6 +28,31 @@ number=68
 print(check_squares(number))
 
 
+#PF-Prac-36
+def find_target_positions(input_string, target_word):
+    target_list=[]
+    temp_list = input_string.split(" ")
+    for i in range(0,len(temp_list)):
+        if(temp_list[i]==target_word):
+            target_list.append(i)
+
+    return target_list
+
+def find_inverted_index(input_string):
+    target_dict={}
+    old_list = input_string.split(" ")
+    for i in old_list:
+        dict = {i:find_target_positions(input_string,i)}
+        target_dict.update(dict)
+
+    return target_dict
+    
+    
+input_string="we dont need no education we dont need no thought control no we dont"
+result_dict=find_inverted_index(input_string)
+print(result_dict
+      
+      
 #PF-Prac-37
 def sum_of_list(num_list):
     if(len(num_list)>1):
