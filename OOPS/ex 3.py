c9c01592a83739d8cb1166c5b4f2200191d36568
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-27 12:12:59 +0530

    Create ex 3.py

---
+++ OOPS/ex 3.py
@@ -0,0 +1,9 @@
+class Employee:
+    def __init__(self, name, age,sal):
+        self.name = name
+        self.age = age
+        self.salary=sal
+        
+
+Jack=Employee("Jack",24,30000)
+Jill=Employee("Jill",27,40000)
