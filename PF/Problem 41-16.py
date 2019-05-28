6388cf2a3dfdbbfa8d09acaec238045c6ad9f742
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-28 14:02:21 +0530

    Update Problem 41-16.py

--- PF/Problem 41-16.py
+++ PF/Problem 41-16.py
@@ -1,4 +1,4 @@
-#PF-Tryout
+#PF-Tryout 46
 
 def doublePreceding (values):
     if len(values) > 0:
@@ -11,3 +11,20 @@ def doublePreceding (values):
     return values
     
 print(doublePreceding([3,8,2]))
+
+
+#46_hackerrank
+b=input()
+b=b.split(" ")
+def double(v):
+    if len(v) > 0:
+        pre = int(v[0])
+        v[0] = 0
+        for i in range(1, len(v)):
+            temp=int(v[i])
+            v[i] = 2 * int(pre)
+            pre = temp
+    return v
+bb=double(b)
+for i in bb:
+    print(i,end=" ")
