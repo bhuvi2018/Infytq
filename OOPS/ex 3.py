76eb6f1343620179db3ba2a1e920d94769212d6b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-28 10:10:39 +0530

    Update and rename ex 3.py to ex 3,4.py

--- OOPS/ex 3.py
+++
@@ -1,9 +0,0 @@
-class Employee:
-    def __init__(self):
-        self.name = name
-        self.age = age
-        self.salary=sal
-        
-
-Jack=Employee("Jack",24,30000)
-Jill=Employee("Jill",27,40000)
