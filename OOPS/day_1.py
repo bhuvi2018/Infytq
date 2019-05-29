b9f9e47719cce3d87c026a4db9a09c60548b221b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-29 17:53:15 +0530

    Update day_1.py

--- OOPS/day_1.py
+++ OOPS/day_1.py
@@ -55,49 +55,60 @@ Ram.purchases()
 Ram.pays_bill()
 
 
-#ASS_5
 class Vehicle:
-    def __init__(self,vehicle_id, vehicle_cost,vehicle_type):
-        self._vehicle_id = vehicle_id
-        self._vehicle_cost=vehicle_cost
-        self._vehicle_type= vehicle_type
+    def __init__(self):
+        self.__vehicle_id = None
+        self.__vehicle_cost=None
+        self.__vehicle_type= None
+        self.__premium_amount=None
         
     def set_vehicle_id(self, vehicle_id):
-            self._vehicle_id = vehicle_id
+            self.__vehicle_id = vehicle_id
 
     def get_vehicle_id(self):
-        return self._vehicle_id
+        return self.__vehicle_id
         
     def set_vehicle_cost(self, vehicle_cost):
-            self._vehicle_cost = vehicle_cost
+            self.__vehicle_cost = vehicle_cost
 
     def get_vehicle_cost(self):
-        return self._vehicle_cost
+        return self.__vehicle_cost
         
     def set_vehicle_type(self, vehicle_type):
-            self._vehicle_type = vehicle_type
+            self.__vehicle_type = vehicle_type
+            
+    def get_premium_amount(self):
+        return self.__premium_amount
+            
+    def set_premium_amount(self,premium_amount):
+            self.__premium_amount = premium_amount
 
     def get_vehicle_type(self):
-        return self._vehicle_type
+        return self.__vehicle_type
     
     def calculate_premium(self):
-        if(self._vehicle_type=="TwoWheeler"):
-            self._premium_amount=self._vehicle_cost*2/100
-        elif(self._vehicle_type=="FourWheeler"):
-            self._premium_amount=self._vehicle_cost*6/100
+        if(self.__vehicle_type=="TwoWheeler"):
+            self.__premium_amount=self.__vehicle_cost*2/100
+            print(self.__premium_amount)
+        elif(self.__vehicle_type=="FourWheeler"):
+            self.__premium_amount=self.__vehicle_cost*6/100
+            print(self.__premium_amount)
         else:
             print("vehicle type is invalid")
     
     def display_vehicle_details(self):
-        print("Vehicle ID:",self._vehicle_id)
-        print("Vehicle Cost:",self._vehicle_cost)
-        print("Vehicle Type:",self._vehicle_type)
-        print("Premium Amount:",self._premium_amount)
+        print("Vehicle ID:",self.__vehicle_id)
+        print("Vehicle Cost:",self.__vehicle_cost)
+        print("Vehicle Type:",self.__vehicle_type)
+        print("Premium Amount:",self.__premium_amount)
     
 
-Car=Vehicle("BM2009",450000,"FouWheeler")
-Car.set_vehicle_type("FourWheeler")
+Car=Vehicle()
+Car.set_vehicle_type("TwoWheeler")
+Car.set_premium_amount(1000)
+Car.set_vehicle_cost(100000)
 Car.calculate_premium()
+Car.set_vehicle_id(111)
 Car.display_vehicle_details()
 
 
