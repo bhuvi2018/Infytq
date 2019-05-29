9157588f3607e0254098bff8a231bd2e2353705b
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-29 14:52:31 +0530

    Update digit_in_a_num.py

--- Digit/digit_in_a_num.py
+++ Digit/digit_in_a_num.py
@@ -10,3 +10,20 @@ while (i>0):
     i=i//10
 if(f==0):
     print("digit not found")
+
+    
+#digits counting
+i=int(input())
+f=0
+c=0
+d=int(input())
+while (i>0):
+    r=i%10
+    if r==d:
+        c+=1
+        f=1
+    i=i//10
+if(f==0):
+    print("digit not found")
+else:
+    print("digit's count:",c)
