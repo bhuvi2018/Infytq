2f07c3b7e9f555d7dde0c9f25a03501980db270f
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-30 20:35:38 +0530

    Update day_2.py

--- OOPS/day_2.py
+++ OOPS/day_2.py
@@ -154,3 +154,27 @@ b.calculate_bill_amount(c,q,p)
 print(b.get_bill_id())
 print(b.get_patient_name())
 print(b.get_bill_amount())
+
+
+#OOPR-Assgn-13
+#Start writing your code here
+class Classroom:
+    classroom_list=['G015', 'G066', 'L123', 'L135', 'L143', 'L13']
+    
+    @staticmethod
+    def search_classroom(class_room):
+        class_room=class_room.upper()
+        f=0
+        s="Found"
+        ss=-1
+        for i in Classroom.classroom_list:
+            if ('L'==i[0] and class_room[0]=='L'):
+                if class_room in i:
+                    return s
+                    f=1
+                    break
+        if(f==0):
+            return ss
+
+c=Classroom()
+print(c.search_classroom('L135'))
