da5827b3dab7724843778add6514d048b5eb655a
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-30 12:59:56 +0530

    Update Problem 11-20.py

--- PF/Problem 11-20.py
+++ PF/Problem 11-20.py
@@ -67,6 +67,23 @@ def find_five_digit():
 
 find_five_digit()
 
+#14 HackerRank
+#PF-Tryout
+def find_five_digit():
+    n2=0
+    n3=0
+    n4=0
+    n5=0
+    for i in range(9,-1,-1):
+        n2=i-2
+        n3=n2-2
+        n4=n3-2
+        n5=n3
+        if((n3+n4+n5)==i and (i+n2+n3+n4+n5)==19):
+            print(str(i)+str(n2)+str(n3)+str(n4)+str(n5))
+            break
+
+find_five_digit()
 
 #PF-Prac-15
 def check_22(num_list):
