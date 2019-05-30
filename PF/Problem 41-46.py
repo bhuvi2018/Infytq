6c668e7502a975c207d80e832415b0630ef1a28e
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-30 11:15:33 +0530

    Update Problem 41-46.py

--- PF/Problem 41-46.py
+++ PF/Problem 41-46.py
@@ -1,3 +1,32 @@
+#PF-Prac-42
+def maxvalue_in_column(pixel_grid):
+    result_list = [0] * len(pixel_grid[0])
+    for item in pixel_grid:
+        temp = item
+        for i in range(0,len(item)):
+            if result_list[i]  < temp[i]:
+                result_list[i] = temp[i]
+    #start writing your code here
+    return result_list
+
+pixel_grid=[[ 4, 2, 3], 
+            [16, 5, 0],
+            [ 3, 200, 6], 
+            [ 0, 10, 12]] 
+pixel_grid1=[[ 4 ], 
+             [ 16 ], 
+             [ 3 ], 
+             [ 0 ]]
+pixel_grid2=[[ 4, 2, 3]]
+pixel_grid3= [[6]] 
+
+print("Pixel grid is:")
+for i in pixel_grid:
+    print(i)
+output=maxvalue_in_column(pixel_grid)
+print("The maximum values of each column are:",output)
+
+
 #PF-Tryout 46
 
 def doublePreceding (values):
