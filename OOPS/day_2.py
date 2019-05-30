398d822fdeebffc4732e19f44d6acedb586566b7
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-30 17:42:21 +0530

    Update day_2.py

--- OOPS/day_2.py
+++ OOPS/day_2.py
@@ -50,3 +50,35 @@ call3='9990000001,9330000003,6,ISD'
 
 list_of_call_string=[call,call2,call3]
 Util().parse_customer(list_of_call_string)
+
+
+#OOPR-Assgn-12
+#Start writing your code here
+class Bill:
+    def __init__(self,bill_id,patient_name):
+        self.__bill_id=bill_id
+        self.__patient_name=patient_name
+        self.__bill_amount=0
+        
+    def get_bill_id(self):
+        return self.__bill_id
+    
+    def get_patient_name(self):
+        return self.__patient_name
+        
+    def get_bill_amount(self):
+        return self.__bill_amount
+        
+    def calculate_bill_amount(self,consultation_fees,quantity_list,price_list):
+        self.__bill_amount=consultation_fees
+        for i in range(0,len(quantity_list)):
+            self.__bill_amount+=(quantity_list[i]*price_list[i])
+    
+b=Bill(20,"John")
+p=[99,88,77,55]
+q=[1,2,3,4]
+c=100
+b.calculate_bill_amount(c,q,p)
+print(b.get_bill_id())
+print(b.get_patient_name())
+print(b.get_bill_amount())
