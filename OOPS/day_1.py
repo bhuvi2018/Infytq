e00c97bd0218ba64bf3a0d5d61cc5a15381fc94f
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-05-30 10:23:06 +0530

    Update day_1.py

--- OOPS/day_1.py
+++ OOPS/day_1.py
@@ -12,6 +12,36 @@ Jill=Employee("Jill",27,40000)
 
 #OOPR-Exer-4
 class Athlete:
+    def __init__(self):
+        self.__name=""
+        self.__gender=""
+    
+    def set_name(self,name):
+        self.__name=name
+    
+    def get_name(self):
+        return self.__name
+        
+    def set_gender(self,gender):
+        self.__gender=gender
+    
+    def get_gender(self):
+        return self.__gender
+        
+    def running(self):
+        if(self.__gender=="girl"):
+            print("150mtr running")
+        else:
+            print("200mtr running")
+k=Athlete()
+k.set_gender("girl")
+k.set_name("Maria")
+k.running()
+   
+    #OR
+    
+#OOPR-Exer-4
+class Athlete:
     def __init__(self,name,gender):
         self.__name=name
         self.__gender=gender
