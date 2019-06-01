14df1ccce109f16345db47cb25296c3cd9a8b040
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-06-01 13:06:05 +0530

    Update day_3.py

--- OOPS/day_3.py
+++ OOPS/day_3.py
@@ -39,3 +39,23 @@ class Ticket:
             
 John=Ticket("John","DelHi","Kolkata")
 John.generate_ticket()
+
+
+#OOPR-Exer-8
+class Juggler:
+    def __init__(self,name):
+        self.__name=name
+
+    def juggles(self,jugglingitem):
+        self.__jugglingitem=jugglingitem
+        print(self.__name ,"is juggling with ",self.__jugglingitem)
+
+class JugglingItem:
+    def __init__(self,name):
+        self.__name=name
+
+    def get_name(self):
+        return self.__name
+s=JugglingItem("Balls")
+Jack=Juggler("Jack")
+Jack.juggles(s.get_name())
