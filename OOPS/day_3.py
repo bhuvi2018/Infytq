f4b4d1861f3085b5e0e84779b936af5509b0e900
bhuvi2018 <36320311+bhuvi2018@users.noreply.github.com>
2019-06-03 20:05:11 +0530

    Update day_3.py

--- OOPS/day_3.py
+++ OOPS/day_3.py
@@ -82,3 +82,46 @@ class Parrot:
         
 p=Parrot("k","red")
 k=Parrot("g","yellow")
+
+
+#OOPR-Assgn-16
+class Customer:
+    def __init__(self,phone_no,name,age):
+        self.phone_no=phone_no
+        self.name=name
+        self.age=age
+        self.list_of_calls=None
+
+class CallDetail:
+    def __init__(self,phone_no,called_no,duration):
+        self.phone_no=phone_no
+        self.called_no=called_no
+        self.duration=duration
+
+class Util:
+    def __init__(self):
+        self.cust_phn=None
+        self.list_of_call=None
+        
+    def parse_customer(self,list_of_customers,list_of_calls):
+        for i in list_of_customers:
+            for j in list_of_calls:
+                self.cust_phn=i.phone_no
+                if i.phone_no==j.phone_no:
+                    self.list_of_call=j.called_no
+                
+cust1=Customer(9900009901,'cust1',23)
+cust2=Customer(9900009902,'cust2',24)
+cust3=Customer(9900009903,'cust3',25)
+list_of_customers=[cust1,cust2,cust3]
+
+call1=CallDetail(9900009901,8800123401,5)
+call2=CallDetail(9900009903,8800123402,10)
+call3=CallDetail(9900009902,8800123403,2)
+call4=CallDetail(9900009901,8800123404,8)
+call5=CallDetail(9900009901,8800123405,7)
+call6=CallDetail(9900009903,8800123406,9)
+call7=CallDetail(9900009903,8800123407,4)
+list_of_calls=[call1,call2,call3,call4,call5,call6,call7]
+
+Util().parse_customer(list_of_customers, list_of_calls)
